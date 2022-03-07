ecd70ecf081ac26a3eb0436319f0661e74df2f46
dollarstir <kpin463@gmail.com>
2022-03-07 22:24:24 +0000

    scsd

--- db.php
+++ db.php
@@ -1,10 +1,3 @@
 <?php
- 
-$conn=mysqli_connect("localhost","root","","hct") or die ("connection failed");
 
-
-
-the name of the soemthing we csall si quiteg  normal to the etnagefngt
-
-
-?>
+$conn = mysqli_connect('localhost', 'root', '', 'hct') or die('connection failed');
